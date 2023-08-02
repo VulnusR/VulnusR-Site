af90ef5b0c811e34581620aa289dd147b226af4f
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-08-01 18:02:45 -0600

    Landing page updated. TODO: Introduce hover effects on the black & white subsections. Make Design Responsive.

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -10,52 +10,40 @@ const LandingPage = () => {
         <div id='landing-content-main-parent'>
           <div id="landing-content-parent">
             <div id="landing-content-parent-left" className='landing-bisection'>
-             
               <div id='landing-main-content-parent-left' className='landing-content'>
                 <div id='landing-upper-parent-left' className='landing-img-section-parent'>
                   <div id='landing-upper-left' className='landing-img-section'>
                     <div id='landing-upper-left-design'></div>
                     <div id='landing-upper-left-img'></div>
                   </div>
+                </div>
+                <div id='landing-lower-parent-left' className='landing-title-text-section'>
+                <h2 id='landing-text-left' className='black-text landing-text'>
+                  UI Design&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  Web Development&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  UX Design
+                </h2>
+
 
                 </div>
-                <div className='landing-lower-parent'>
-
-                </div>
-                
               </div>
-              
             </div>
             <div id="landing-content-parent-right" className='landing-bisection'>
-            
               <div id='landing-main-content-parent-right' className='landing-content'>
-                <div id='landing-upper-parent-right'>
+                <div id='landing-upper-parent-right' className='landing-title-text-section'>
+                  <h1 id='landing-text-right' className='white-text landing-text'>Caleb S. Lockwood</h1>
                   
 
                 </div>
                 <div id='landing-lower-parent-right' className='landing-img-section-parent'>
-                  <div id='landing-lower-right' className='landing-img-section' >
+                  <div id='landing-lower-right' className='landing-img-section'>
                     <div id='landing-lower-right-design'></div>
                     <div id='landing-lower-right-img'></div>
                   </div>
-
                 </div>
-                
               </div>
-              
-            
             </div>
           </div>
-
-
-          
-
-          
-
-
-          
-
-         
         </div>
       </div>
     </div>
